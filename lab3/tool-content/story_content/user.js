2b8a8a9b27a68eb382f0fe44a253b5463991aa6b
Georgina Davies <georginadavies1701@gmail.com>
2023-09-17 13:28:28 -0700

    lab3upload

---
+++ lab3/tool-content/story_content/user.js
@@ -0,0 +1,348 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5to1h2Gpvjc":
+        Script1();
+        break;
+      case "6fjduX4YE9F":
+        Script2();
+        break;
+      case "6jSMp2LXFLb":
+        Script3();
+        break;
+      case "5sT8YrEiHKC":
+        Script4();
+        break;
+      case "6lukgDmFjlt":
+        Script5();
+        break;
+      case "5X2srVvktbu":
+        Script6();
+        break;
+      case "5kk6ULdZ8st":
+        Script7();
+        break;
+      case "6XyLMH5KOGL":
+        Script8();
+        break;
+      case "660aYKjQmc2":
+        Script9();
+        break;
+      case "6CkxVM6esFi":
+        Script10();
+        break;
+      case "64ceuetEshQ":
+        Script11();
+        break;
+      case "6naAlgTqnis":
+        Script12();
+        break;
+      case "5rddyYp4B8B":
+        Script13();
+        break;
+      case "6DTzqkFoxGd":
+        Script14();
+        break;
+      case "6WvZdDdIGWv":
+        Script15();
+        break;
+      case "5m85mMQtjrd":
+        Script16();
+        break;
+      case "6nvrg9ERaNS":
+        Script17();
+        break;
+      case "6nIpWVJC1kw":
+        Script18();
+        break;
+      case "6JiW9vYIVAJ":
+        Script19();
+        break;
+      case "6Ww4e7sxpbe":
+        Script20();
+        break;
+      case "6GxXb53U6mG":
+        Script21();
+        break;
+      case "60RgwO43otb":
+        Script22();
+        break;
+      case "6q6CPQ8u3vM":
+        Script23();
+        break;
+      case "5vVLWEMNzjr":
+        Script24();
+        break;
+      case "60MEK42RYeG":
+        Script25();
+        break;
+      case "6UCKEougTUe":
+        Script26();
+        break;
+      case "6CvFMBRqcTS":
+        Script27();
+        break;
+      case "5XWp7RvCxdH":
+        Script28();
+        break;
+      case "5jK55R8OCJ2":
+        Script29();
+        break;
+      case "6Wlok6FqbIP":
+        Script30();
+        break;
+      case "6Eb5euITPFh":
+        Script31();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script17()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script18()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script19()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script20()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script21()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script22()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script23()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script24()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script25()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script26()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script27()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script28()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script29()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script30()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+
+function Script31()
+{
+  var player = GetPlayer();
+var totalSlides = player.GetVar("TotalSlides");
+var slideNumberText = "Slide " + player.slideIndex + " of " + totalSlides;
+player.SetVar("SlideNumberText", slideNumberText);
+}
+

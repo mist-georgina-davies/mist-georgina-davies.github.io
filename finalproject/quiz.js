6e605f900e243d891909974d5fa9f9070b17533e
Georgina Davies <georginadavies1701@gmail.com>
2023-10-16 11:33:32 -0700

    update quiz.js

--- finalproject/quiz.js
+++ finalproject/quiz.js
@@ -1,14 +1,17 @@
 function checkAnswers() {
     var score = 0;
+
     var q1Answer = document.querySelector('input[name="q1"]:checked').value;
     var q2Answer = document.querySelector('input[name="q2"]:checked').value;
-    
-    // Add more variables as per your question count
+    var q3Answer = document.querySelector('input[name="q3"]:checked').value;
+    var q4Answer = document.querySelector('input[name="q4"]:checked').value;
+    var q5Answer = document.querySelector('input[name="q5"]:checked').value;
 
     if (q1Answer === "c") { score++; }
     if (q2Answer === "a") { score++; }
-    
-    // Add more condition checks as per your question count
+    if (q3Answer === "b") { score++; }
+    if (q4Answer === "d") { score++; }
+    if (q5Answer === "a") { score++; }
 
-    document.getElementById("result").innerHTML = "You scored " + score + " out of 2!";
+    document.getElementById("result").innerHTML = "You scored " + score + " out of 5!";
 }
